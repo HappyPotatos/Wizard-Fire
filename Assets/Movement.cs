d01a18552baaba7ae31d907cb55b4596ec537d0e
Yung Kim <z5361816@ad.unsw.edu.au>
2024-07-13 23:53:23 +1000

    i don twanna

---
+++ Assets/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * 5);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * 5);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * 5);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * 5);
+        }
+    }
+}
