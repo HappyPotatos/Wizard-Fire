3a0fad6afdc02a75911bf7f3ffa1662e72da184c
Samantha Tse <z5363701@unsw.edu.au>
2024-07-16 23:01:20 +1000

    left time

--- Assets/moveLeft.cs
+++ Assets/moveLeft.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveLeft : MonoBehaviour
+public class MoveLeft : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.Translate(Vector2.left * Time.deltaTime * 5);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.left * Time.deltaTime * 5);
     }
 }
