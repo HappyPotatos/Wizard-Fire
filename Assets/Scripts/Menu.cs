438de3e407a9f0da12b10ef02223293470767d57
Samantha Tse <z5363701@unsw.edu.au>
2024-07-28 23:43:34 +1000

    why is making a menu screen so hard

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public Button playButton; // Assign the play button in the Unity editor
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(PlayGame); // Add a listener for the button click event
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("GameScene"); // Replace "GameScene" with the name of your game scene
+        StartCoroutine(UnloadMainMenu()); // Unload the main menu scene after a short delay
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single); // Load the game scene and unload the previous scene
+
+        // Log the state of each scene before loading the game scene
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            Debug.Log($"Scene: {scene.name}, IsLoaded: {scene.isLoaded}");
+        }
+    }
+
+    private IEnumerator UnloadMainMenu()
+    {
+        yield return new WaitForSeconds(0.1f); // Wait for a short period of time
+        SceneManager.UnloadSceneAsync("MainMenu"); // Unload the main menu scene
+
+        // Log the state of each scene after unloading the main menu scene
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            Debug.Log($"Scene: {scene.name}, IsLoaded: {scene.isLoaded}");
+        }
     }
 
     public void SelectLevel()
