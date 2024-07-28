c1189cd7d449e68d3db5b3ec0bc062fbcb43fd54
Samantha Tse <z5363701@unsw.edu.au>
2024-07-28 23:59:20 +1000

    quit button works

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,36 +6,29 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     public Button playButton; // Assign the play button in the Unity editor
+    public Button quitButton; // Assign the quit button in the Unity editor
 
     private void Start()
     {
-        playButton.onClick.AddListener(PlayGame); // Add a listener for the button click event
+        playButton.onClick.AddListener(PlayGame); // Add a listener for the play button click event
+        quitButton.onClick.AddListener(QuitGame); // Add a listener for the quit button click event
     }
 
     public void PlayGame()
     {
         StartCoroutine(UnloadMainMenu()); // Unload the main menu scene after a short delay
         SceneManager.LoadScene("GameScene", LoadSceneMode.Single); // Load the game scene and unload the previous scene
-
-        // Log the state of each scene before loading the game scene
-        for (int i = 0; i < SceneManager.sceneCount; i++)
-        {
-            Scene scene = SceneManager.GetSceneAt(i);
-            Debug.Log($"Scene: {scene.name}, IsLoaded: {scene.isLoaded}");
-        }
     }
 
     private IEnumerator UnloadMainMenu()
     {
         yield return new WaitForSeconds(0.1f); // Wait for a short period of time
         SceneManager.UnloadSceneAsync("MainMenu"); // Unload the main menu scene
+    }
 
-        // Log the state of each scene after unloading the main menu scene
-        for (int i = 0; i < SceneManager.sceneCount; i++)
-        {
-            Scene scene = SceneManager.GetSceneAt(i);
-            Debug.Log($"Scene: {scene.name}, IsLoaded: {scene.isLoaded}");
-        }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     public void SelectLevel()
@@ -46,10 +39,5 @@ public class Menu : MonoBehaviour
     public void Credits()
     {
         // Implement credits logic here, such as loading a credits scene or panel
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
     }
 }
