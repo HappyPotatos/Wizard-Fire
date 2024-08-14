caa511db00f65ec38da4f1379253001aa988dc74
Samantha Tse <z5363701@unsw.edu.au>
2024-08-14 22:18:12 +1000

    menu screen readjust

--- Assets/Scripts/BackgroundMovement.cs
+++ Assets/Scripts/BackgroundMovement.cs
@@ -4,19 +4,19 @@ public class BackgroundMovement : MonoBehaviour
 {
     public float speed = 1f; // The speed at which the background moves
 
-    private Renderer _renderer;
+    private SpriteRenderer _spriteRenderer;
 
     void Start()
     {
-        _renderer = GetComponent<Renderer>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         // Calculate the new position by moving the current position to the left
-        Vector2 newPosition = new Vector2(_renderer.material.mainTextureOffset.x - (speed * Time.deltaTime), 0);
+        Vector2 newPosition = new Vector2(_spriteRenderer.material.mainTextureOffset.x - (speed * Time.deltaTime), 0);
 
         // Update the texture offset
-        _renderer.material.mainTextureOffset = newPosition;
+        _spriteRenderer.material.mainTextureOffset = newPosition;
     }
 }
